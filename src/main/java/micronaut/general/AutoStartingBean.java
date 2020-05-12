d97df0d2dc5bb1c4fa1fef9b61c19e15b11ae32e
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2020-05-12 10:11:28 +0200

    upgrade to Micronaut 2.0.0.M3

--- src/main/java/micronaut/general/AutoStartingBean.java
+++ src/main/java/micronaut/general/AutoStartingBean.java
@@ -2,7 +2,7 @@ package micronaut.general;
 
 import io.micronaut.context.event.StartupEvent;
 import io.micronaut.runtime.event.annotation.EventListener;
-import io.micronaut.spring.tx.annotation.Transactional;
+import io.micronaut.transaction.annotation.TransactionalAdvice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,7 +10,7 @@ import javax.inject.Singleton;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-@Singleton @Transactional
+@Singleton @TransactionalAdvice
 public class AutoStartingBean {
 
 	public static final Logger logger = LoggerFactory.getLogger(AutoStartingBean.class);
